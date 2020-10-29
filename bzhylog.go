3efe0381d8b0a98bd042a86c160047dc660e1547
Wayne.Wang <net_use@bzhy.com>
2020-10-29 14:39:12 +0800

    bzhylog logger

--- bzhylog.go
+++ bzhylog.go
@@ -2,42 +2,92 @@ package bzhyserver
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/wangyysde/bzhylog"
 )
 
 // LogFormatter gives the signature of the formatter function passed to LoggerWithFormatter
-//type LogFormatter func(params LogFormatterParams) string
+type LogFormatter func(params LogFormatterParams) string
 
-/*
 // LogFormatterParams is the structure any formatter will be handed when time to log comes
 type LogFormatterParams struct {
-   Request *http.Request
-
-  // TimeStamp shows the time after the server returns a response.
-  TimeStamp time.Time
-  // StatusCode is HTTP response code.
-  StatusCode int
-  // Latency is how much time the server cost to process a certain request.
-  Latency time.Duration
-  // ClientIP equals Context's ClientIP method.
-  ClientIP string
-  // Method is the HTTP method given to the request.
-  Method string
-  // Path is a path the client requests.
-  Path string
-  // ErrorMessage is set if error has occurred in processing the request.
-  ErrorMessage string
- // isTerm shows whether does gin's output descriptor refers to a terminal.
-  isTerm bool
- // BodySize is the size of the Response Body
- BodySize int
- // Keys are the keys set on the request's context.
- Keys map[string]interface{}
+	Request *http.Request
+	// TimeStamp shows the time after the server returns a response.
+  	TimeStamp time.Time
+  	// StatusCode is HTTP response code.
+  	StatusCode int
+  	// Latency is how much time the server cost to process a certain request.
+  	Latency time.Duration
+  	// ClientIP equals Context's ClientIP method.
+  	ClientIP string
+  	// Method is the HTTP method given to the request.
+  	Method string
+  	// Path is a path the client requests.
+  	Path string
+  	// ErrorMessage is set if error has occurred in processing the request.
+  	ErrorMessage string
+ 	// isTerm shows whether does gin's output descriptor refers to a terminal.
+  	isTerm bool
+ 	// BodySize is the size of the Response Body
+ 	BodySize int
+ 	// Keys are the keys set on the request's context.
+ 	Keys map[string]interface{}
 }
-*/
+
+// StatusCodeColor is the ANSI color for appropriately logging http status code to a terminal.
+func (p *LogFormatterParams) StatusCodeColor() string {
+    code := p.StatusCode
+
+    switch {
+        case code >= http.StatusOK && code < http.StatusMultipleChoices:
+            return green
+        case code >= http.StatusMultipleChoices && code < http.StatusBadRequest:
+            return white
+        case code >= http.StatusBadRequest && code < http.StatusInternalServerError:
+            return yellow
+        default:
+            return red
+    }
+}
+
+// MethodColor is the ANSI color for appropriately logging http method to a terminal.
+func (p *LogFormatterParams) MethodColor() string {
+        method := p.Method
+
+        switch method {
+        case http.MethodGet:
+                return blue
+        case http.MethodPost:
+                return cyan
+        case http.MethodPut:
+                return yellow
+        case http.MethodDelete:
+                return red
+        case http.MethodPatch:
+                return green
+        case http.MethodHead:
+                return magenta
+        case http.MethodOptions:
+                return white
+        default:
+                return reset
+        }
+}
+
+// ResetColor resets all escape attributes.
+func (p *LogFormatterParams) ResetColor() string {
+        return reset
+}
+
+// IsOutputColor indicates whether can colors be outputted to the log.
+func (p *LogFormatterParams) IsOutputColor() bool {
+        return consoleColorMode == forceColor || (consoleColorMode == autoColor && p.isTerm)
+}
+
 
 type bzhyLoggerConfig struct {
 	//The path of access log file
@@ -54,14 +104,18 @@ type bzhyLoggerConfig struct {
 	AccFd *os.File
 	//The error log file descriptor
 	ErrFd *os.File
-
+        
 	Formatter LogFormatter
+ 
+       // SkipPaths is a url path array which logs are not written.
+       // Optional.
+        SkipPaths []string
 }
 
-var LoggerConf bzhyLoggerConfig = bzhyLoggerConfig{"", "", nil, nil, nil, nil, nil, nil}
+var LoggerConf bzhyLoggerConfig = bzhyLoggerConfig{"", "", nil, nil, nil, nil, nil, nil,nil}
 
 // defaultLogFormatter is the default log format function Logger middleware uses.
-/*
+
 var defaultLogFormatter = func(param LogFormatterParams) string {
 	var statusColor, methodColor, resetColor string
 	if param.IsOutputColor() {
@@ -74,8 +128,8 @@ var defaultLogFormatter = func(param LogFormatterParams) string {
 		// Truncate in a golang < 1.8 safe way
 		param.Latency = param.Latency - param.Latency%time.Second
 	}
-	return fmt.Sprintf(" %v |%s %3d %s| %13v | %15s |%s %-7s %s %#v\n%s",
-		param.TimeStamp.Format("2006/01/02 - 15:04:05"),
+	return fmt.Sprintf("%s %3d %s| %13v | %15s |%s %-7s %s %#v\n%s",
+//		param.TimeStamp.Format("2006/01/02 - 15:04:05"),
 		statusColor, param.StatusCode, resetColor,
 		param.Latency,
 		param.ClientIP,
@@ -84,7 +138,7 @@ var defaultLogFormatter = func(param LogFormatterParams) string {
 		param.ErrorMessage,
 	)
 }
-*/
+
 
 // Create a new instance of the logger for StdOut.
 func CreateStdLog() {
@@ -271,32 +325,31 @@ func WriteLog2Errlog(msg string, level string) (ret int) {
 
 // Logger instances a Logger middleware that will write the logs to gin.DefaultWriter.
 // By default gin.DefaultWriter = os.Stdout.
-/*
+
 func Logger() HandlerFunc {
 	//Initating StdOut Logger
 	if LoggerConf.StdLog == nil {
 		CreateStdLog()
 	}
 
-	if LoggerConf.AccLog == nil && len(LoggerConf.AccLogFile) < 1 {
-		ret := 0
-		ret = CreateAccLog(LoggerConf.AccLogFile)
-		if ret > 0 {
-			LoggerConf.AccLog = LoggerConf.StdLog
-		}
-	}
+        if LoggerConf.AccLog == nil || LoggerConf.AccLog == LoggerConf.StdLog {
+             OpenAccessLogger("")
+        }
 
-	if LoggerConf.ErrLog == nil && len(LoggerConf.ErrLogFile) < 1 {
-		ret := 0
-		ret = CreateErrLog(LoggerConf.ErrLogFile)
-		if ret > 0 {
-			LoggerConf.ErrLog = LoggerConf.StdLog
-		}
-	}
+
+        if LoggerConf.ErrLog == nil || LoggerConf.ErrLog == LoggerConf.StdLog {
+              OpenErrorLogger("")
+        } 
+         
 
 	if LoggerConf.Formatter == nil {
 		LoggerConf.Formatter = defaultLogFormatter
 	}
+       
+        isTerm := true
+        
+        var skip map[string]struct{}
+
 	return func(c *Context) {
 		// Start timer
 		start := time.Now()
@@ -331,7 +384,7 @@ func Logger() HandlerFunc {
 
 			param.Path = path
 
-			if param.ErrorMessage != nil {
+			if len(param.ErrorMessage) > 0  {
 				LogError2StdAndFile(LoggerConf.Formatter(param), "error")
 			} else {
 				LogAccess2StdAndFile(LoggerConf.Formatter(param), "info")
@@ -340,7 +393,7 @@ func Logger() HandlerFunc {
 	}
 
 }
-*/
+
 
 // Set Access logfile and create Access logger
 func OpenAccessLogger(AccessLogFile string) (ret int) {
@@ -357,9 +410,6 @@ func OpenAccessLogger(AccessLogFile string) (ret int) {
 	}
 
 	if LoggerConf.AccLog == nil {
-		if LoggerConf.StdLog == nil {
-			CreateStdLog()
-		}
 		LoggerConf.AccLog = LoggerConf.StdLog
 	}
 
@@ -381,15 +431,48 @@ func OpenErrorLogger(ErrorLogFile string) (ret int) {
 	}
 
 	if LoggerConf.ErrLog == nil {
-		if LoggerConf.ErrLog == nil {
-			CreateStdLog()
-		}
 		LoggerConf.ErrLog = LoggerConf.StdLog
 	}
 
 	return 0
+}
 
+//Set Access file to LoggerConf
+func SetAccessFile(AccessFile string) (ret int) {
+	if len(AccessFile) < 1 {
+		LogError2StdAndFile(fmt.Sprintf("Access file path is NULL"),"warn")
+		return 200003
+	}
+	if strings.Compare(LoggerConf.AccLogFile,AccessFile) != 0 {
+		AccFd, err := os.OpenFile(AccessFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
+		if err != nil {
+			LogError2StdAndFile(fmt.Sprintf("Failed to open the ACCESS log file %s Error message: %s", AccessFile, err), "fatal")
+			return 200004
+		}
+		AccFd.Close()
+		OpenAccessLogger(AccessFile)
+		return 0
+	} 
+	
 	return 0
 }
 
-//
+//Set Error file to LoggerConf
+func SetErrorFile(ErrorFile string) (ret int) {
+	if len(ErrorFile) < 1 {
+		LogError2StdAndFile(fmt.Sprintf("Error file path is NULL"),"warn")
+		return 200005
+	}
+	if strings.Compare(LoggerConf.ErrLogFile,ErrorFile) != 0 {
+		ErrFd, err := os.OpenFile(ErrorFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
+		if err != nil {
+			LogError2StdAndFile(fmt.Sprintf("Failed to open the ERROR log file %s Error message: %s", ErrorFile, err), "fatal")
+			return 200004
+		}
+		ErrFd.Close()
+		OpenErrorLogger(ErrorFile)
+		return 0
+	} 
+	
+	return 0
+}
